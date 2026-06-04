02ca334cbfb798b428cc48ef1016c2572425b994
Mawunyo Hafoba <mawunyohafoba28@gmail.com>
2026-06-04 20:19:14 +0000

    Capacitorjs In the flesh

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,15 +1,13 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'com.dismed.app',
-  appName: 'Dismed',
-  webDir: '.out/',
-  server: {
-    url: "https://dismed.vercel.app" ,
-    cleartext: false
-
-  }
-
+	appId: "com.dismed.app",
+	appName: "Dismed",
+	webDir: ".out/",
+	server: {
+		url: "https://dismed.vercel.app",
+		cleartext: false,
+	},
 };
 
 export default config;
